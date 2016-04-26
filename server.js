f00e558f332c54c8efa0a623185f1b3d7e25bc0e
Azlan Jamal <akmalazlanjamal@gmail.com>
2016-04-26 14:58:45 +0800

    Add route for create and get users.

--- server.js
+++ server.js
@@ -43,6 +43,60 @@ app.get('/', function (req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// route middleware and first route are here
+
+// on routes that end in /users
+// ----------------------------
+apiRouter.route('/users')
+
+    // create a user (accessed at POST http://localhost:1234/api/users)
+    .post(function (req, res) {
+
+        // create a new instance of the User model
+        var user = new User();
+
+        // set the users information (comes from the request)
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
+
+        // save the user and check for errors
+        user.save(function (err) {
+            if (err) {
+                // duplicate entry
+                if (err.code == 11000) {
+                    return res.json({ success: false, message: 'A user with that username already exists.' });
+                } else {
+                    return res.send(err);
+                }
+            }
+            res.json({ message: 'User created!' });
+        });
+    })
+
+    // get all the users (accessed at GET http://localhost:1234/api/users)
+    .get(function (req, res) {
+        User.find(function (err, users) {
+            if (err) res.send(err);
+
+            // return the users
+            res.json(users);
+        });
+    });
+
+apiRouter.route('/users/:user_id')
+
+    // get the user with that id
+    // (accessed at GET http://localhost:1234/api/users/:user_id)
+    .get(function (req, res) {
+        User.findById(req.params.user_id, function (err, user) {
+            if (err) res.send(err);
+
+            // return that user
+            res.json(user);
+        });
+    });
+
 // middleware to use for all requests
 apiRouter.use(function (req, res, next) {
     // do logging
