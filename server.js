7acc16d3f0135ab6b1b269724235354940ec4cfa
Azlan Jamal <akmalazlanjamal@gmail.com>
2016-04-26 14:26:57 +0800

    Configure route.

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var morgan     = require('morgan'); // used to see requests
 var mongoose   = require('mongoose'); // for working w/ our database
 var port       = process.env.PORT || 1234; // set the port for our app
 
+var User = require('./app/models/user');
+
+//connect to our database (hosted locally)
+mongoose.connect('mongodb://localhost:27017/meanapidb');
+
 // APP CONFIGURATION ---------
 // use body parser so we can grab information from POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,12 +38,24 @@ app.get('/', function (req, res) {
     res.send('Welcome to the home page!');
 });
 
+
+
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// middleware to use for all requests
+apiRouter.use(function (req, res, next) {
+    // do logging
+    console.log('Somebody just came to our app!');
+
+    // we'll add more to the middleware in Chapter 10
+    // this is where we will authenticate users
+
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working
 // accessed at GET http://localhost:1234/api
-
 apiRouter.get('/', function (req, res) {
     res.json(({ message: 'hooray! welcome to our api!' }));
 });
